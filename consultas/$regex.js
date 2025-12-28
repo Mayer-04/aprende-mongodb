50d105e5c1cbdb06c099471b8000a1f2c9c0d0b4
Mayer04 <andres.20.chaves@gmail.com>
2025-12-28 16:43:58 -0500

    chore: formatear código

--- consultas/$regex.js
+++ consultas/$regex.js
@@ -1,19 +1,19 @@
 // $regex: buscar documentos usando expresiones regulares
 
 // Buscar nombres que comiencen con "A"
-db.users.find({ name: { $regex: "^A" } })
+db.users.find({ name: { $regex: "^A" } });
 
 // Búsqueda case-insensitive (mayúsculas/minúsculas):
-db.users.find({ name: { $regex: "^a", $options: "i" } })
+db.users.find({ name: { $regex: "^a", $options: "i" } });
 
 // Buscar nombres que terminen con "ez":
-db.users.find({ name: { $regex: "ez$" } })
+db.users.find({ name: { $regex: "ez$" } });
 
 // Buscar nombres que contengan "and":
-db.users.find({ name: { $regex: "and", $options: "i" } })
+db.users.find({ name: { $regex: "and", $options: "i" } });
 
 // Sintaxis alternativa con barras:
-db.users.find({ name: /^A/i })
+db.users.find({ name: /^A/i });
 
 // Opciones comunes:
 // i = case-insensitive
